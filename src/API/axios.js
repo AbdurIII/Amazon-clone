b920a69412675300e217c8abc2237a2db3ae2b88
AbdurIII <abdurox5@gmail.com>
2025-09-24 02:26:52 +0300

    update

--- src/API/axios.js
+++ src/API/axios.js
@@ -4,7 +4,6 @@ const axiosInstance = axios.create({
   //for local server firebase function
   // baseURL: "http://127.0.0.1:5001/clone-24281/us-central1/api",
   // deployed version of amazon server in render.com
-  // baseURL: "https://amazon-backend-deploy-dotb.onrender.com/",
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:5050",
 });
 export { axiosInstance };
