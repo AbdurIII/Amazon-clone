5ed370a3d31aa02ba46f067c883b28dab9362170
AbdurIII <abdurox5@gmail.com>
2025-09-24 18:41:30 +0300

    frontend deployment on netlify

--- src/API/axios.js
+++ src/API/axios.js
@@ -5,6 +5,7 @@ const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-24281/us-central1/api",
   // deployed version of amazon server in render.com
   baseURL: "https://amazon-backend-api-wqyq.onrender.com",
+  // to run on local server 
   // baseURL: "http://localhost:5050",
 });
 export { axiosInstance };
